12679804b0ef4049f94727a8c0905f4eed665b81
yzh <yzh.han@outlook.com>
2026-01-23 17:02:15 +0000

    m

--- src/driftguard/model/training/trainer.py
+++ src/driftguard/model/training/trainer.py
@@ -272,7 +272,7 @@ class Trainer:
                     device_type=self.device.type,
                     enabled=self.scaler is not None,
                 ):
-                    out, l1_w, l2_w = self.model(inputs)
+                    out, l1_w, l2_w = self.model(inputs), None, None
                     loss = self.loss_fn(out, targets)
 
                 # 记录 loss
