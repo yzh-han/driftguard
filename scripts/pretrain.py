bc80428235dc96e7f542745f08f1ffa85c572999
yzh <yzh.han@outlook.com>
2026-01-23 21:44:32 +0000

    m

--- scripts/pretrain.py
+++ scripts/pretrain.py
@@ -13,7 +13,7 @@ from torchvision.models import resnet18
 import json
 from pathlib import Path
 
-for ds in [DATASET.DG5, DATASET.PACS, DATASET.DDN]:
+for ds in [DATASET.PACS, DATASET.DDN]: # DATASET.DG5
     path, n_class, img_size = ds.value
     domain = json.loads(Path(path).read_text())["domains"][0]
     data_path = Path(path).parent / domain
@@ -23,8 +23,8 @@ for ds in [DATASET.DG5, DATASET.PACS, DATASET.DDN]:
     full_ds = datasets.ImageFolder(data_path, transform=val_tfm)
 
     idxs = random.sample(range(len(full_ds)), 300)
-    train_idxs_1, train_idxs_2, val_idxs  = (
-        idxs[:100], idxs[100:200], idxs[200:300]
+    train_idxs_1, train_idxs_2, val_idxs, train_idx  = (
+        idxs[:100], idxs[100:200], idxs[200:300], idxs[:200]
     )
 
     train_ds, val_ds = (
@@ -32,18 +32,20 @@ for ds in [DATASET.DG5, DATASET.PACS, DATASET.DDN]:
         datasets.ImageFolder(data_path, transform=val_tfm)
     )
 
-    train_loader_11, train_loader_12, train_loader_21, train_loader_22, test_loader = (
+    train_loader_11, train_loader_12, train_loader_21, train_loader_22, train_loader, test_loader = (
         DataLoader(Subset(val_ds, train_idxs_1), batch_size=16, shuffle=True),
         DataLoader(Subset(train_ds, train_idxs_1), batch_size=16, shuffle=True),
         DataLoader(Subset(val_ds, train_idxs_2), batch_size=16, shuffle=True),
         DataLoader(Subset(train_ds, train_idxs_2), batch_size=16, shuffle=True),
+        DataLoader(Subset(val_ds, train_idx), batch_size=32, shuffle=True),
         DataLoader(Subset(val_ds, val_idxs), batch_size=16, shuffle=False),
     )
     # print("train_1:", Counter(train_ds_1.targets[i] for i in train_idxs_1))
     # print("train_2:", Counter(train_ds_2.targets[i] for i in train_idxs_2))
     # break
 
-    for model in [MODEL.CRST_S, MODEL.CRST_M, MODEL.CVIT]:
+    # MODEL.CRST_S,
+    for model in [ MODEL.CRST_M, MODEL.CVIT]:
         
         if model == MODEL.CRST_S and (ds == DATASET.PACS or ds == DATASET.DDN):
             continue  # skip cresnet_s on pacs and ddn
@@ -70,6 +72,7 @@ for ds in [DATASET.DG5, DATASET.PACS, DATASET.DDN]:
 
         history = trainer.fit(train_loader_21, test_loader)
         history = trainer.fit(train_loader_22, test_loader)
+        history = trainer.fit(train_loader, test_loader)
         trainer.save()
 
         
