b2ba42405c0b37c3d7261be992382a30e5af7fb6
yzh <yzh.han@outlook.com>
2026-01-26 22:04:33 +0000

    m

--- scripts/mk_expdir.sh
+++ scripts/mk_expdir.sh
@@ -3,3 +3,9 @@ mkdir DDN-crst_m
 mkdir DDN-cvit
 mkdir PACS-crst_m
 mkdir PACS-cvit
+
+mv DG5-crst_s-* DG5-crst_s/
+mv DDN-crst_m-* DDN-crst_m/
+mv DDN-cvit-* DDN-cvit/
+mv PACS-crst_m-* PACS-crst_m/
+mv PACS-cvit-* PACS-cvit/
