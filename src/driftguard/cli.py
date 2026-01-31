a4d342166ae1d75694d3f2e5cf5752f77cf46b51
yzh <yzh.han@outlook.com>
2026-01-31 00:00:42 +0000

    m

--- src/driftguard/cli.py
+++ src/driftguard/cli.py
@@ -96,23 +96,24 @@ exps = Exps(
     datasets=[
         # DATASET.DG5, 
         DATASET.PACS, 
-        # DATASET.DDN
+        DATASET.DDN
     ],
     models=[
         # MODEL.CRST_S, 
         MODEL.CRST_M, 
-        # MODEL.CVIT
+        MODEL.CVIT
     ],
     strategies=[
-        # Never(),
-        # AveTrig(thr_acc=0.65, data_port=11101, server_port=11102),
-        # PerCTrig(thr_acc=0.65, data_port=11201, server_port=11202),
-        # MoEAve(thr_acc=0.65, data_port=11301, server_port=11302),
-        # MoEPerC(thr_acc=0.65, data_port=11401, server_port=11402),
-        # Cluster(thr_acc=0.65, data_port=11501, server_port=11502),
+        Never(),
+        AveTrig(thr_acc=0.6, data_port=11101, server_port=11102),
+        PerCTrig(thr_acc=0.6, data_port=11201, server_port=11202),
+        MoEAve(thr_acc=0.6, data_port=11301, server_port=11302),
+        MoEPerC(thr_acc=0.6, data_port=11401, server_port=11402),
+        Cluster(thr_acc=0.6, data_port=11501, server_port=11502),
+        Driftguard(thr_group_acc=0.6, data_port=11601, server_port=11602),
         # Driftguard(thr_reliance=0.35, thr_group_acc=0.65, data_port=11701, server_port=11702, name="reliance_35"),
         # Driftguard(thr_reliance=0.25, thr_group_acc=0.65, data_port=11601, server_port=11602, name="Dri_rel25"),
-        Driftguard(thr_reliance=0.3, thr_group_acc=0.65, data_port=12801, server_port=12802, name="DRI_rel30"),
+        # Driftguard(thr_reliance=0.3, thr_group_acc=0.65, data_port=12801, server_port=12802, name="DRI_rel30"),
         # Driftguard(thr_reliance=0.4, thr_group_acc=0.65, data_port=11901, server_port=11902, name="reliance_40"),
     ],
     device = "cuda:1" if torch.cuda.is_available() else "cpu", # <--------------------
@@ -127,7 +128,8 @@ def main() -> None:
         clustr = str(cluster_thr).split('.')[0] + str(cluster_thr).split('.')[-1]
         cfg = LaunchConfig(
             # exp_root=f"exp/ablation_{exp.strategy.name}",
-            exp_root=f"exp/{exp.strategy.name}_clu{clustr}_mgsize{min_group_size}",
+            # exp_root=f"exp/{exp.strategy.name}_clu{clustr}_mgsize{min_group_size}",
+            exp_root="exp/main_acc60",
             exp_name=exp.name,
             # data service
             sample_size_per_step = 30,
