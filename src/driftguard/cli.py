d1412f22de1d1d26e27528bc57968806d6403dcc
yzh <yzh.han@outlook.com>
2026-02-07 23:21:47 +0000

    m

--- src/driftguard/cli.py
+++ src/driftguard/cli.py
@@ -106,15 +106,21 @@ exps = Exps(
     ],
     strategies=[
         # ablation
-        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.99, cluster_thr= 0.2, min_group_size=2, data_port=14611, server_port=14612),
-        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=2, data_port=14621, server_port=14622),
-        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.9, cluster_thr= 0.2, min_group_size=2, data_port=14631, server_port=14632),
-        
+        # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.99, cluster_thr= 0.2, min_group_size=2, data_port=14611, server_port=14612),
+        # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=2, data_port=14621, server_port=14622),
+        # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.9, cluster_thr= 0.2, min_group_size=2, data_port=14631, server_port=14632),
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.85, cluster_thr= 0.2, min_group_size=2, data_port=11631, server_port=11632),
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.8, cluster_thr= 0.2, min_group_size=2, data_port=12631, server_port=12632),
+
         # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.1, min_group_size=2, data_port=14641, server_port=14642),
         # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.3, min_group_size=2, data_port=14651, server_port=14652),
-
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.4, min_group_size=2, data_port=13651, server_port=13652),
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.5, min_group_size=2, data_port=14651, server_port=14652),
+        
         # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=1, data_port=14661, server_port=14662),
         # Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=3, data_port=14671, server_port=14672),
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=4, data_port=15671, server_port=15672),
+        Driftguard(thr_group_acc=0.6, thr_sha_acc_pct=0.95, cluster_thr= 0.2, min_group_size=5, data_port=16671, server_port=16672),
 
         # Never(),
         # AveTrig(thr_acc=0.85, data_port=13101, server_port=13102),
